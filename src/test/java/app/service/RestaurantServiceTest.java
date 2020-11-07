b3b94e6074a4d1f846ea71dd1c159c5df0ca0c25
Roman Seropyan <seropyanry@gmail.com>
2020-11-07 22:10:34 +0300

    getByNonExistingId() method refactored

--- src/test/java/app/service/RestaurantServiceTest.java
+++ src/test/java/app/service/RestaurantServiceTest.java
@@ -2,34 +2,31 @@ package app.service;
 
 import app.entity.Restaurant;
 import app.exceptions.EntityNotFoundException;
-import org.assertj.core.api.Assertions;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static app.service.testdata.RestaurantTestData.testDataRestaurant;
+import static app.service.testdata.RestaurantTestData.testRestaurant1;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RestaurantServiceTest extends ServiceTest {
 
     @Autowired
     private RestaurantService restaurantService;
 
-    @Rule
-    public ExpectedException thrownException = ExpectedException.none();
-
     @Test
     public void getById() {
-        Restaurant realRestaurant = restaurantService.getById(1);
-        Assertions.assertThat(realRestaurant).usingRecursiveComparison().isEqualTo(testDataRestaurant);
+        Restaurant realRestaurant1 = restaurantService.getById(1);
+        assertThat(realRestaurant1).usingRecursiveComparison().isEqualTo(testRestaurant1);
     }
 
     @Test
     public void getByNonExistingId() {
         int id = -1;
-        thrownException.expect(EntityNotFoundException.class);
-        thrownException.expectMessage("Restaurant with id=" + id + " not found.");
-        restaurantService.getById(id);
+        assertThatThrownBy(() -> {
+            restaurantService.getById(id);
+        }).isInstanceOf(EntityNotFoundException.class)
+                .hasMessage("Restaurant with id=" + id + " not found.");
     }
 
 }
--- src/test/java/app/service/testdata/RestaurantTestData.java
+++ src/test/java/app/service/testdata/RestaurantTestData.java
@@ -9,22 +9,22 @@ import java.util.List;
 
 public class RestaurantTestData {
 
+    public static final Restaurant testRestaurant1;
     private static final Meal meal1;
     private static final Meal meal2;
     private static final Meal meal3;
     private static final Meal meal4;
     private static final Meal meal5;
-    public static final Restaurant testDataRestaurant;
 
     static {
-        testDataRestaurant = new Restaurant(1, "Marcellis", 3);
-        meal1 = new Meal(1, "Ceaser Salad", MealCategory.SALAD, 350, testDataRestaurant);
-        meal2 = new Meal(2, "Tomato Soup", MealCategory.SOUP, 290, testDataRestaurant);
-        meal3 = new Meal(3, "Four Seasons pizza", MealCategory.MAIN, 560, testDataRestaurant);
-        meal4 = new Meal(4, "Apple pie", MealCategory.DESERT, 250, testDataRestaurant);
-        meal5 = new Meal(5, "Latte", MealCategory.DRINK, 350, testDataRestaurant);
+        testRestaurant1 = new Restaurant(1, "Marcellis", 3);
+        meal1 = new Meal(1, "Ceaser Salad", MealCategory.SALAD, 350, testRestaurant1);
+        meal2 = new Meal(2, "Tomato Soup", MealCategory.SOUP, 290, testRestaurant1);
+        meal3 = new Meal(3, "Four Seasons pizza", MealCategory.MAIN, 560, testRestaurant1);
+        meal4 = new Meal(4, "Apple pie", MealCategory.DESERT, 250, testRestaurant1);
+        meal5 = new Meal(5, "Latte", MealCategory.DRINK, 350, testRestaurant1);
         List<Meal> meals = Arrays.asList(meal1, meal2, meal3, meal4, meal5);
-        testDataRestaurant.setMeals(meals);
+        testRestaurant1.setMeals(meals);
     }
 
 }
