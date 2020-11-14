c81475e58e9ff18f33696e837567e8cec774e065
Roman Seropyan <seropyanry@gmail.com>
2020-11-14 22:22:09 +0300

    Spring profiles related issue fixed for test classes

--- src/test/java/app/service/ServiceTest.java
+++ src/test/java/app/service/ServiceTest.java
@@ -1,12 +1,14 @@
 package app.service;
 
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
+@ActiveProfiles("dev")
 @ContextConfiguration(classes = {app.config.AppConfig.class, app.config.DbConfig.class})
 @Sql(scripts = "/db_population.sql")
 @Transactional
