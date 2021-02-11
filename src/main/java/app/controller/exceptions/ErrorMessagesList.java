27ec1d451bae08a0ad79c8624f4d1406140ac3cf
Roman Seropyan <seropyanry@gmail.com>
2021-02-11 22:29:12 +0300

    ErrorInfo renamed to ErrorMessagesList and completely refactored

---
+++ src/main/java/app/controller/exceptions/ErrorMessagesList.java
@@ -0,0 +1,28 @@
+package app.controller.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ErrorMessagesList {
+
+    @JsonProperty("errors")
+    private final List<String> errorMessages;
+
+    public ErrorMessagesList() {
+        this.errorMessages = new ArrayList<>();
+    }
+
+    public void addErrorMessage(String message) {
+        errorMessages.add(message);
+    }
+
+    public void addErrorMessage(Exception e) {
+        errorMessages.add(e.getMessage());
+    }
+
+    public List<String> getErrorMessages() {
+        return errorMessages;
+    }
+}
